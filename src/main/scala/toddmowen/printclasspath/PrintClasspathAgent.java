8814309430a8fb84a4e39f6f6aca48d6304a547b
Todd Owen <todd.m.owen@gmail.com>
2015-10-21 03:43:29 +1100

    Print classpath.

--- src/main/scala/toddmowen/printclasspath/PrintClasspathAgent.java
+++ src/main/scala/toddmowen/printclasspath/PrintClasspathAgent.java
@@ -1,9 +1,16 @@
 package toddmowen.printclasspath;
 
 import java.lang.instrument.Instrumentation;
+import java.net.URL;
+import java.net.URLClassLoader;
 
 public class PrintClasspathAgent {
     public static void premain(String agentArgs, Instrumentation inst) {
-	System.out.println("premain");
+	URLClassLoader classLoader = (URLClassLoader) ClassLoader.getSystemClassLoader();
+
+	System.out.println("[print-classpath]");
+	for (URL url: classLoader.getURLs()) {
+	    System.out.println("    " + url.getFile());
+	}
     }
 }
